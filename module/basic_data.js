fa3b05c90b10df8e25876633caadf35063721e79
KwakYounHo <kwakyh951@gmail.com>
2023-03-20 20:27:24 +0900

    CommonJS, ESM 모양새 보기

---
+++ module/basic_data.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//   a : '이것 a데이터 입니다.'
+// }
+
+/*---------같은 모양새----------*/
+// commonJS 방식
+const a = {
+  a : '이것 a데이터 입니다.'
+}
+// module.exports = a;
+
+/*-------ES방식---------*/
+// 모듈 방식
+export default a;
