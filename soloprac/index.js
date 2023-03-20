f6de0bc3b7916191038742d9516debb4eff7ff47
KwakYounHo <kwakyh951@gmail.com>
2023-03-20 20:29:09 +0900

    데이터 베이스 테이블을 fs모듈로 JSON파일로 만들어 저장하기

--- soloprac/index.js
+++ soloprac/index.js
@@ -1,23 +1,24 @@
-// const mysql = require('mysql2');
-// const conn = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   port     : '3306',
-//   database : 'test_youn'
-// })
-// conn.connect();
-// let command = `select * from testtable3`;
-// conn.query(
-//   command,
-//   (err,result,fields)=>{
-//     if (err) throw err;
-//     console.log(result);
-//     console.log('?');
-//   }
-// );
-// conn.end();
+import mysql from 'mysql2'
+import fs from 'fs'
+const conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  port     : '3306',
+  database : 'test_youn'
+})
+conn.connect();
+let command = `select * from testtable3`;
+conn.query(
+  command,
+  (err,result,fields)=>{
+    if (err) throw err;
+    console.log(result);
+    fs.writeFileSync('./'+'select_result'+'.json',JSON.stringify(result));
+  }
+);
+conn.end();
 
-const p     = document.createElement('p');
-p.innerHTML = 'hello';
-document.body.appendChild(p);
+// const p     = document.createElement('p');
+// p.innerHTML = 'hello';
+// document.body.appendChild(p);
